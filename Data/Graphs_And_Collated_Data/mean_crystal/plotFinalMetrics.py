486474b4da25eebe65c0417b40d2ba40f0e5afb3
mholman5721 <mholman@alumni.uidaho.edu>
2020-05-12 19:10:59 -0700

    Agent_Communication: more data
    5-12-2020 7:11 p.m.

---
+++ Data/Graphs_And_Collated_Data/mean_crystal/plotFinalMetrics.py
@@ -0,0 +1,43 @@
+# Matthew Holman        5-12-2020
+# Agent Communication
+#
+# Final Metrics Plotter
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Labels
+xLabel = "Relative Rankings"
+yLabel = "Averages for All Tests"
+saveTitle = "Final_Metrics"
+graphTitle = "Mean Crystals Collected"
+
+# Read in data
+data_csv = pd.read_csv("Mean_Crystal_Comparison.csv", index_col="Strategy")
+dataT = data_csv.transpose()
+
+# Create subplot
+fmfig, fm = plt.subplots() # Random Crystals, Random Start
+
+# Plot data
+fm.plot(dataT["Communication"], color="blue", marker="o", label="Communication")
+fm.plot(dataT["Non-Communication"], color="green", marker="*", label="Non-Communication")
+fm.plot(dataT["Brute Force Detecting"], color="orange", marker="+", label="Brute Force Detecting")
+fm.plot(dataT["Brute Force Non-Detecting"], color="red", marker="x", label="Brute Force Non-Detecting")
+
+# Change plot size
+box = fm.get_position()
+fm.set_position([box.x0, box.y0 + box.height * 0.2, box.width, box.height * 0.8])
+
+# Place the legend
+fm.legend(loc='upper center', bbox_to_anchor=(0.5, -0.18), fancybox=True, shadow=True, ncol=2)
+
+# Set the title
+fm.set_title(graphTitle)
+
+# Set the axis labels
+fm.set_xlabel(xLabel)
+fm.set_ylabel(yLabel)
+
+# Show the graph
+plt.show()
